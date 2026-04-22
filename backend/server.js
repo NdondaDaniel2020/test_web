43ff478c3329586e7b89b9cc83108f6e18ea983f
NdondaDaniel2020 <ndondadaniel2020@gmail.com>
2026-04-22 10:24:22 +0100

    feat: Inicialização do backend com Prisma e Express
    - Adicionado package.json para as dependências do backend, incluindo Prisma, Express e dotenv.
    
    - Criado prisma.config.ts para a configuração do Prisma com variável de ambiente para a URL do banco de dados.
    
    - Adicionado schema.prisma definindo o modelo User com MongoDB como fonte de dados.
    
    - Implementado server.js com rotas Express para gerenciamento de usuários (criar, atualizar, excluir, listar).
    
    - Incluído package-lock.json para bloqueio de dependências.

---
+++ backend/server.js
@@ -0,0 +1,49 @@
+import express from 'express';
+import cors from 'cors';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+const app = express();
+const port = 3000;
+const USERS = [];
+
+app.use(cors());
+app.use(express.json());
+
+// rota de teste
+app.get('/', (req, res) => {
+  res.send('welcome');
+})
+
+// criar um novo usuário
+app.post('/user', async (req, res) => {
+  const user = await prisma.user.create({data: req.body})
+  res.status(201).json(user);
+});
+
+// editar usuario
+app.put('/user', async (req, res) => {
+  const user = await prisma.user.update({
+    where: { email: req.body.email },
+    data: req.body
+  });
+  res.json(user);
+});
+
+// deletar usuario
+app.delete('/user', async (req, res) => {
+  const user = await prisma.user.delete({
+    where: { email: req.body.email }
+  });
+  res.json(user);
+});
+
+// listar todos os usuários
+app.get('/users', async (req, res) => {
+    const users = await prisma.user.findMany();
+    res.json(users);
+});
+
+app.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
